118c0810c5e66eccc0af6bfb80b9296628c2a535
Oscar González <oscar.gonzalezmartinez@gmail.com>
2020-03-30 20:13:28 +0200

    Separando nombres

--- ms-2/src/main/java/org/ogm/gkedemo2/controller/Doit.java
+++ ms-2/src/main/java/org/ogm/gkedemo2/controller/Doit.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -17,8 +18,8 @@ public class Doit {
 	@Value("${ms1-endpoint}")
 	private String ms1Endpoint = null;
 	
-	@RequestMapping("/add/{first}/{second}")
-	public ResponseEntity<Integer> add(@PathVariable("first") String first,@PathVariable("second") String second) {
+	@RequestMapping("/add")
+	public ResponseEntity<Integer> add(@RequestParam("first") String first,@RequestParam("second") String second) {
 		
 		RestTemplate restTemplate = new RestTemplate();
 		String resourceUrl = ms1Endpoint+"/add/"+first+"/"+second;
